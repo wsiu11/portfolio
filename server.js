aac357a4e599316826262dcd9d33733c55a04efa
wsiu11 <wsiu11@my.centennialcollege.ca>
2023-10-05 15:48:31 -0400

    update default port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('./config/express');
 
 var app = express();
-app.listen(3000);
+app.listen(8080);
 module.exports = app;
 
-console.log('Server running at http://localhost:3000/');
+console.log('Server running at http://localhost:8080/');
